1c417bf53de2df1d565de8d6c4e0beb713144c88
Iwona Sasak <sasak.iwona.88@gmail.com>
2018-02-26 22:49:38 +0100

    First scenario fixed

--- src/test/java/stepdefs/UserCreationSteps.java
+++ src/test/java/stepdefs/UserCreationSteps.java
@@ -1,33 +1,40 @@
 package stepdefs;
 
+import java.util.List;
+
+import cucumber.api.DataTable;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import elements.Credentials;
 
 public class UserCreationSteps {
 
 	@Given("^The user is on (.*) page$")
 	public void userIsOnUserCreationPage(String pageTitle) {
-
+		System.out.println("pageTitle: " + pageTitle);
 	}
 
 	@Given("^Username and password is set to:$")
-	public void userCredentialsAreSetTo(String username, String password) throws Throwable {
-
+	public void userCredentialsAreSetTo(DataTable credsDataTable) throws Throwable {
+		Credentials creds = credsDataTable.asList(Credentials.class).get(0);
+		System.out.println("user inputs username and pass: " + creds.getUsername() + " " + creds.getPassword());
 	}
 
 	@When("^The user inputs valid username and password:$")
-	public void userInputsValidUsernameAndPassword(String username, String password) throws Throwable {
-
+	public void userInputsValidUsernameAndPassword(DataTable credsDataTable) throws Throwable {
+		Credentials creds = credsDataTable.asList(Credentials.class).get(0);
+		System.out.println("user inputs username and pass: " + creds.getUsername() + " " + creds.getPassword());
 	}
 
 	@When("^The user clicks (.*) button$")
 	public void userClicksTheButton(String buttonName) {
-
+		System.out.println("button clicked: " + buttonName);
 	}
 
 	@Then("^The user can see her/his credentials set correctly above the form:$")
-	public void userCanSeeHisCredentialsOnTheForm(String username, String password) throws Throwable {
-
+	public void userCanSeeHisCredentialsOnTheForm(DataTable credsDataTable) throws Throwable {
+		Credentials creds = credsDataTable.asList(Credentials.class).get(0);
+		System.out.println("user inputs username and pass: " + creds.getUsername() + " " + creds.getPassword());
 	}
 }
--- src/test/resources/features/userCreation.feature
+++ src/test/resources/features/userCreation.feature
@@ -3,16 +3,20 @@ Feature: Creating a new user on the thedemosite.
   with username and password with more than 4 characters.
 
   Scenario: A user can set valid credentials correctly
-    Given The user is on <Add a User> page
+    Given The user is on Add a User page
     And Username and password is set to:
-      | test | test |
+      | username | password |
+      | test  | test  |
     When The user inputs valid username and password:
-      | testiwi | testiwi |
-    And The user clicks <Save> button
+      | username | password |
+      | testiwi  | testiwi  |
+    And The user clicks Save button
     Then The user can see her/his credentials set correctly above the form:
-      | testiwi | testiwi |
+      | username | password |
+      | testiwi  | testiwi  |
 
   Scenario: A user can not use username that is shorter than 4 characters
+    Given The user is on Add a User page
 
   Scenario: A user can not use password that is shorter than 4 characters
 
