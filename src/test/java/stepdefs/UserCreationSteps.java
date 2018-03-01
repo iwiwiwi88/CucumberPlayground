47d3f47d48af9a8ec54f169cf950d67cfed0585a
Iwona Sasak <sasak.iwona.88@gmail.com>
2018-03-02 00:08:20 +0100

    Base class of PageObject created

--- src/test/java/stepdefs/UserCreationSteps.java
+++ src/test/java/stepdefs/UserCreationSteps.java
@@ -12,8 +12,8 @@ import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import helpers.Credentials;
-import helpers.DriverHelper;
+import pageobjects.Credentials;
+import pageobjects.DriverHelper;
 
 public class UserCreationSteps {
 
